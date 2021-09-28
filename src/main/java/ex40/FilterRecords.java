1cfdfe78f49c124d2e05268c78b73e81635935a6
JeremyR <kidnip@knight.ucf.edu>
2021-09-27 23:27:06 -0400

    Second commit

--- src/main/java/ex40/FilterRecords.java
+++ src/main/java/ex40/FilterRecords.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
 public class FilterRecords
 {
     public static Employee[] sortList(Employee[] employees, int number){
